f7401562aee54025b6c5282f81afb2f4518fa8ad
Fozkun <fozkun230403@gmail.com>
2025-01-13 11:01:03 +0700

    update coupons

---
+++ app/src/main/java/com/rmit/android_tiramisu_vacation_rental/enums/CouponType.java
@@ -0,0 +1,5 @@
+package com.rmit.android_tiramisu_vacation_rental.enums;
+
+public enum CouponType {
+    PERCENTAGE_DISCOUNT
+}
---
+++ app/src/main/java/com/rmit/android_tiramisu_vacation_rental/models/CouponModel_Tri.java
@@ -0,0 +1,17 @@
+package com.rmit.android_tiramisu_vacation_rental.models;
+
+import com.rmit.android_tiramisu_vacation_rental.enums.CouponType;
+
+import java.util.Date;
+
+public class CouponModel_Tri {
+    private String id;
+    private String title;
+    private String description;
+    private CouponType type;
+    private String value;
+    private Date createdDate;
+
+    public CouponModel_Tri() {
+    }
+}
